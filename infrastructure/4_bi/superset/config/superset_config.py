659b3eab0849ef46ec791c592911b99df578e1d8
ThanhTinhDE <thanhtinh.de@gmail.com>
2026-05-13 14:53:34 +0200

    merger config + add accept frame

--- infrastructure/4_bi/superset/config/superset_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Database Metadata của Superset (Dùng chung Postgres với Warehouse)
-SQLALCHEMY_DATABASE_URI = 'postgresql://dwh_admin:dwh_admin@123@postgres-warehouse:5432/superset_metadata'
-
-SECRET_KEY = 'your-secret-key-here-very-secret'
-WTF_CSRF_ENABLED = False
-ENABLE_CORS = True
-MAPBOX_API_KEY = ''
