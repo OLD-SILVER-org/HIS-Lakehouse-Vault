2d5451c79423bbdf0f06b1c89cc5b743b99deb5e
ThanhTinhDE <thanhtinh.de@gmail.com>
2026-06-04 11:22:48 +0200

    re make snapshot DDL

--- infrastructure/3_warehouse/postgres/sql/create_staging_metadata.sql
+++ infrastructure/3_warehouse/postgres/sql/create_staging_metadata.sql
@@ -5,16 +5,16 @@
 CREATE SCHEMA IF NOT EXISTS staging_metadata;
 
 CREATE TABLE IF NOT EXISTS staging_metadata.snapshot_tracker (
-  lake_table_name VARCHAR(255) PRIMARY KEY,
-  last_snapshot_id BIGINT,
-  last_load_time TIMESTAMP
+  lake_table_name  VARCHAR(255) PRIMARY KEY,
+  last_load_time   TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS staging_metadata.load_tracker (
-  lake_table_name VARCHAR(255),
-  load_id BIGINT,
-  load_time TIMESTAMP,
-  status VARCHAR(50),
-  records_loaded BIGINT,
-  error_message TEXT
+CREATE TABLE  IF NOT EXISTS staging_metadata.load_tracker (
+  id               BIGSERIAL PRIMARY KEY,  -- tự sinh, không cần truyền vào
+  lake_table_name  VARCHAR(255),
+  load_type        VARCHAR(20),            -- INIT / INCREMENTAL
+  load_time        TIMESTAMP,
+  status           VARCHAR(20),            -- SUCCESS / FAILED
+  batch_size   BIGINT,
+  error_message    TEXT
 );
